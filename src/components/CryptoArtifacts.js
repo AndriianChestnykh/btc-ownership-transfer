541e332378ea253b14134db59d99eb73c5947e1a
Andriian Chestnykh <achestnykh@provectus.com>
2019-09-26 13:01:57 +0300

    Add bcoin

--- src/components/CryptoArtifacts.js
+++ src/components/CryptoArtifacts.js
@@ -1,7 +1,15 @@
 import React from 'react';
+// const bcoin = require('bcoin').set('testnet');
+// const Mnemonic = bcoin.hd.Mnemonic;
+// const mnemonic = new Mnemonic({bits: 256});
 
 function CryptoArtifacts() {
-  return <div>CryptoArtifacts</div>
+  return (<div>
+    <form>
+        <label htmlFor="mnemonic"></label><br/>
+        <textarea id="mnemonic" name="mnemonic" cols="30" rows="3">Put your mnemonic here</textarea>
+    </form>
+  </div>)
 }
 
 export default CryptoArtifacts;
